895cff0e5fce2b1898639ce1a7cfc660a4c413c7
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-10 23:12:23 +0100

    task100

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,56 +1,18 @@
 #!/usr/bin/env bash
-# SET UP WEB SERVERS FOR THE DEPLOYMENT OF web_static
-
-# Update package list and install Nginx
-sudo apt-get update
-sudo apt-get install -y nginx
-
-# Allow HTTP traffic through UFW
-sudo ufw allow 'Nginx HTTP'
-
-# Create directory structure and index.html
-HTML_CONTENT="<html>
+# Setup nginx web server for the deployment of webv_static
+apt update -y
+apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
   <head>
   </head>
   <body>
-    Holberton School
+    <p>Nginx server test</p>
   </body>
-</html>"
-
-WEB_STATIC_DIR="/data/web_static"
-sudo mkdir -p "$WEB_STATIC_DIR"/{releases/test,shared}
-echo "$HTML_CONTENT" | sudo tee "$WEB_STATIC_DIR"/releases/test/index.html > /dev/null
-
-# Delete the existing symbolic link if it exists
-sudo rm -f "$WEB_STATIC_DIR"/current
-
-# Create a new symbolic link to current
-sudo ln -s "$WEB_STATIC_DIR"/releases/test/ "$WEB_STATIC_DIR"/current
-
-# Ensure that the sites-enabled directory exists
-sudo mkdir -p /etc/nginx/sites-enabled/
-
-# Change ownership to ubuntu user
-sudo chown -R ubuntu:ubuntu "$WEB_STATIC_DIR"
-
-# Configure Nginx
-NGINX_CONFIG="
-server {
-    listen 80;
-    listen [::]:80;
-    server_name 100.26.20.18;
-
-    location /hbnb_static/ {
-        alias $WEB_STATIC_DIR/current/;
-    }
-
-    location / {
-        root /usr/share/nginx/html;
-        index index.html index.htm;
-    }
-}
-"
-
-echo "$NGINX_CONFIG" | sudo tee /etc/nginx/sites-available/hbnb_static > /dev/null
-sudo ln -sf /etc/nginx/sites-available/hbnb_static /etc/nginx/sites-enabled/
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
