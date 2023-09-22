956b83f048347e0a949db063b7f5ac5beef1d714
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-22 16:43:31 +0100

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-# A script that stts a flask web application
+""" A script that start a flask application running"""
+
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello():
+    """print Hello"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000, debug=True)
