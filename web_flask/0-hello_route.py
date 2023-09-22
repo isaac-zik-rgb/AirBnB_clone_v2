1fd4b133d310beeed94f30af21a58ab37710d002
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-22 16:46:39 +0100

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,12 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello():
     """print Hello"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
