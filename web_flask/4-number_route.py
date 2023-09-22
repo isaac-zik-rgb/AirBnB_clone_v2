2a74bb1b89d72d027287a6fc3c0f4023b0a020b7
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-22 23:09:28 +0100

    task4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """display Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Display HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_is_cool(text):
+    """ Display 'C' followed by the value of the text"""
+    return "C {}".format(text.replace('_', " "))
+
+
+@app.route('/python/(<text>)')
+def python_is_cool(text="is cool"):
+    """ dispaly python followed by the value of the text"""
+    return "Python {}".format(text.replace('_', " "))
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    return "{:d} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
