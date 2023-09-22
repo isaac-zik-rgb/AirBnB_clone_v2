7afdb5a7d02ad0ac250445c07c3a69d7e6b8ccb7
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-22 16:23:35 +0100

    task0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# A script that stts a flask web application
+from flask import Flask
+""" start up a flask application running on root"""
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
