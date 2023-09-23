2ca5e8bd61c5a064b60cf116647eb1b0bac19b35
Isaac Castro <isaacokechukwu200021@gmail.com>
2023-09-23 18:40:31 +0100

    done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,6 +6,7 @@ from models.state import State
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.teardown_appcontext
 def close_db(exc):
     """ close the current season of sqlalchemy"""
@@ -14,12 +15,12 @@ def close_db(exc):
 
 @app.route('/states_list')
 def display_state():
-     """
+    """
     Displays an HTML page with a list of all State objects in DBStorage.
     States are sorted by name.
     """
-     states = storage.all(State).values()
-     return render_template('7-states_list.html', states=states)
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
